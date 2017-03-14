33eab650f37d2c6f557189667f5a3ef64afc3f37
balachandar <balachandar@versatile-soft.com>
2017-03-14 12:50:56 +0530

    First commit

---
+++ app/src/main/java/com/hindu/tamilnews/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hindu.tamilnews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+String name="";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
